e8bc49b99c56c5c26a00cd680bfc8438a6cfcced
Sven Thiele <sthiele78@gmail.com>
2021-11-17 00:53:46 +0100

    add scripts to switch between dark and bright theme

---
+++ bin/go-bright.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+lookandfeeltool -a org.kde.breezetwilight.desktop
+echo "[Settings]
+gtk-application-prefer-dark-theme=0
+gtk-button-images=1
+gtk-cursor-theme-name=breeze_cursors
+gtk-decoration-layout=:minimize,maximize,close
+gtk-enable-animations=1
+gtk-fallback-icon-theme=breeze
+gtk-font-name=Noto Sans,  12
+gtk-icon-theme-name=breeze
+gtk-menu-images=1
+gtk-primary-button-warps-slider=0
+gtk-theme-name=Adwaita
+gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ" > ~/.config/gtk-3.0/settings.ini
+
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var allDesktops = desktops();
+    print (allDesktops);
+    for (i=0;i<allDesktops.length;i++) {{
+        d = allDesktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                     "org.kde.image",
+                                     "General");
+        d.writeConfig("Image", "file://home/sthiele/Pictures/low_poly-abstract-inverted.jpg")
+    }}
+'
+kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "file://home/sthiele/Pictures/1920x1200-light.jpg"
+
+# set geary back to default
+rm ~/.config/geary/user-style.css
